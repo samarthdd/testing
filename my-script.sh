001d45d0d8f499ccb8626846cd60433dc1b8b9d3
samarthdd <samarth@smalldaytech.com>
2020-12-16 22:17:24 +0530

    testing

--- my-script.sh
+++ my-script.sh
@@ -1,10 +1,51 @@
 #!/bin/bash
 
-GIT_REPOSITORY_URL="https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git"
+set -euo pipefail
 
+function debug() {
+    echo "::debug file=${BASH_SOURCE[0]},line=${BASH_LINENO[0]}::$src"
+}
+
+function warning() {
+    echo "::warning file=${BASH_SOURCE[0]},line=${BASH_LINENO[0]}::$src"
+}
+
+function error() {
+    echo "::error file=${BASH_SOURCE[0]},line=${BASH_LINENO[0]}::$src"
+}
+
+function add_mask() {
+    echo "::add-mask::$doc"
+}
+
+if [ -z "$GITHUB_ACTOR" ]; then
+    error "GITHUB_ACTOR environment variable is not set"
+    exit 1
+fi
+
+if [ -z "$GITHUB_REPOSITORY" ]; then
+    error "GITHUB_REPOSITORY environment variable is not set"
+    exit 1
+fi
+
+if [ -z "$GH_PERSONAL_ACCESS_TOKEN" ]; then
+    error "GH_PERSONAL_ACCESS_TOKEN environment variable is not set"
+    exit 1
+fi
+
+src=doc
+add_mask "${GH_PERSONAL_ACCESS_TOKEN}"
+
+if [ -z "${WIKI_COMMIT_MESSAGE:-}" ]; then
+    debug "WIKI_COMMIT_MESSAGE not set, using default"
+    WIKI_COMMIT_MESSAGE='Automatically publish wiki'
+fi
+
+GIT_REPOSITORY_URL="https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git"
 
 debug "Checking out wiki repository"
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+
 (
     cd "$tmp_dir" || exit 1
     git init
@@ -13,12 +54,25 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
     git pull "$GIT_REPOSITORY_URL"
 ) || exit 1
 
-debug "Enumerating contents of $1"
-for file in $(find $1 -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';'); do
-    debug "Copying $file"
-    cp "$1/$file" "$tmp_dir"
-done
+debug "Enumerating contents of $src"
+STRING="Creating_OVA"
 
+printf 'Enumerating contents of' "$src"
+for folder in $(find $src -maxdepth 1 -execdir basename '{}' ';' | sort )  ; do
+  printf '%s\n' "$folder"
+
+for file in $(find "$src/$folder" -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';' | sort ); do
+    if [[ "$file" == *"$STRING"* ]];then
+    printf '%s\n' "$file"
+    else
+    debug "Copying $file"
+    printf '%s\n' "$src/$folder/$file"
+    cat "$src/$folder/$file" >> wiki_test2.md
+    echo '' >> wiki_test2.md
+    cp wiki_test2.md "$tmp_dir"
+    fi
+done
+done
 debug "Committing and pushing changes"
 (
     cd "$tmp_dir" || exit 1
