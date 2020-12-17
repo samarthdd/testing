3a286ed29682614b04c1c89ef4c1ad1576d6e779
samarthdd <samarth@smalldaytech.com>
2020-12-17 20:26:45 +0530

    testing

--- my-script.sh
+++ my-script.sh
@@ -84,11 +84,9 @@ debug "Committing and pushing changes"
     cd "$tmp_dir" || exit 1
 
     git checkout -b $IMAGE_TAG
-    git config user.email github-actions@github.com
-    git config user.name github-actions
     git add .
     git commit -m "$WIKI_COMMIT_MESSAGE"
-    git push --set-upstream "$GIT_REPOSITORY_URL" $IMAGE_TAG
+    git push origin $IMAGE_TAG
 
     # create a pull request from a new branch to target branch, merge the PR and delete the source branch.
     gh pr create --base $TAG --title "Updated wiki" --body ""
