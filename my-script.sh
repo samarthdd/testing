8cbd1b7f5830964fcae82f306a66c4f0da821f38
samarthdd <samarth@smalldaytech.com>
2020-12-17 21:29:16 +0530

    testing

--- my-script.sh
+++ my-script.sh
@@ -87,11 +87,11 @@ debug "Committing and pushing changes"
 
     git add .
     git commit -m "$WIKI_COMMIT_MESSAGE"
+    git remote add origin "$GIT_REPOSITORY_URL"
     git push --set-upstream "$GIT_REPOSITORY_URL" $IMAGE_TAG
+
     git request-pull $IMAGE_TAG "$GIT_REPOSITORY_URL" master
 
-    git remote add origin "$GIT_REPOSITORY_URL"
-    git push --set-upstream "$GIT_REPOSITORY_URL" master
     git checkout master
     git merge $IMAGE_TAG
 
